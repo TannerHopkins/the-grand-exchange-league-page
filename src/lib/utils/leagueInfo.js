23cb12edab69763a19068b7fcb3c9e95d78f5b91
Tanner Hopkins <jtannerhopkins@gmail.com>
2023-08-20 18:42:07 -0500

    update manager pic

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -26,7 +26,7 @@ export const managers = [
     // "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
     "location": "Nashville, TN (CT)", // (optional)
     "bio": "League Commish. Just here to max my team. IGN: priffindor (ironman).",
-    // "photo": "/managers/priffindor.jpg", // square ratio recommended (no larger than 500x500)
+    "photo": "/managers/shadowwyrm.jpg", // square ratio recommended (no larger than 500x500)
     "fantasyStart": 2009, // (optional) when did the manager start playing fantasy football
     "favoriteTeam": "dal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     // "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
